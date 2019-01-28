adb2283028785d3d0f84c183cbab317613b90bab
Nikko Bustillo <>
2019-01-28 18:16:15 -0500

    JQuery added and functional

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.other-links a').first().delay(900).animate({'opacity':'1'},3500);
+    $('#logo').delay(800).animate({'opacity':'1'},800);
+    $('.navi').delay(1200).animate({'opacity':'1'},1000);
+});
