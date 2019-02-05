a5fe97d18a087191be5b3dbf40769ff12feac4c0
Nikko Bustillo <>
2019-02-05 07:43:56 -0500

    JQuery effects added to info.html :

--- assets/main.js
+++ assets/main.js
@@ -5,5 +5,9 @@ $( document ).ready(function() {
     $('.navi').delay(1200).animate({'opacity':'1'},1000);
     $('#gate').delay(800).animate({'opacity':'1'},1000);
     $('#construction').delay(800).animate({'opacity':'1'},1000);
+    $('#world').delay(700).animate({'opacity':'1'},1000);
+    $('#bio').delay(1800).animate({'opacity':'1'},1000);
+    $('#ig').delay(2800).animate({'opacity':'1'},1000);
+    $('#email').delay(3800).animate({'opacity':'1'},1000);
     
 });
