17983d783a5971c03f117dd49a20e312578a566e
Nikko Bustillo <>
2019-02-05 07:13:33 -0500

    info completed, main.cssre-worked

--- assets/main.js
+++ assets/main.js
@@ -3,4 +3,7 @@ $( document ).ready(function() {
     $('.other-links a').first().delay(900).animate({'opacity':'1'},3500);
     $('#logo').delay(800).animate({'opacity':'1'},800);
     $('.navi').delay(1200).animate({'opacity':'1'},1000);
+    $('#gate').delay(800).animate({'opacity':'1'},1000);
+    $('#construction').delay(800).animate({'opacity':'1'},1000);
+    
 });
