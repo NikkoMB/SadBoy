db8f80e71d15ca1993574813694e21fb4c5bd276
Nikko Bustillo <>
2019-02-07 18:32:30 -0500

    about.html fixed up.

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
     $('.other-links a').first().delay(900).animate({'opacity':'1'},3500);
     $('#logo').delay(800).animate({'opacity':'1'},800);
-    $('.navi').delay(1200).animate({'opacity':'1'},1000);
+    $('.navi').delay(1500).animate({'opacity':'1'},1000);
     $('#gate').delay(800).animate({'opacity':'1'},1000);
     $('#construction').delay(800).animate({'opacity':'1'},1000);
     $('#world').delay(700).animate({'opacity':'1'},1000);
