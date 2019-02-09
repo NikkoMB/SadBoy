674a8f2fc6f8ff1527b9582d2ac26b02900d3d49
Nikko Bustillo <>
2019-02-09 00:40:37 -0500

    link color fixed

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
     $('.other-links a').first().delay(900).animate({'opacity':'1'},3500);
     $('#logo').delay(800).animate({'opacity':'1'},800);
     $('.navi').delay(1500).animate({'opacity':'1'},1000);
+    $('#shopping').delay(1500).animate({'opacity':'1'},1000);
     $('#gate').delay(800).animate({'opacity':'1'},1000);
     $('#construction').delay(800).animate({'opacity':'1'},1000);
     $('#world').delay(700).animate({'opacity':'1'},1000);
